248ee6f92cbdac3a7a575003e37a94a53c34ab56
sunisakc <sunisa_kan@gmail.com>
2018-01-08 15:56:11 +0100

    First Submit

--- SchoolForum/SchoolForum/Models/AccountViewModels.cs
+++ SchoolForum/SchoolForum/Models/AccountViewModels.cs
@@ -18,7 +18,7 @@ namespace SchoolForum.Models
     public class SendCodeViewModel
     {
         public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; } 
         public string ReturnUrl { get; set; }
         public bool RememberMe { get; set; }
     }
