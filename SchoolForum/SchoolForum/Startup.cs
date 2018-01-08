fb050e0abb6902374d225f8e7732ece6112c8857
Taleb01 <taleb_n1@hotmail.com>
2018-01-08 15:46:31 +0100

    SetUpProject.

---
+++ SchoolForum/SchoolForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolForum.Startup))]
+namespace SchoolForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
