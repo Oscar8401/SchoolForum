bcfbfd60422b3fdaec72821d77fd7e41489738c8
sunisakc <sunisa_kan@gmail.com>
2018-01-10 15:43:12 +0100

    Add Categories Model

---
+++ SchoolForum/SchoolForum/Models/Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolForum.Models
+{
+    public class Categories
+    {
+        public int Id{ get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Members{ get; set; }
+    }
+}
