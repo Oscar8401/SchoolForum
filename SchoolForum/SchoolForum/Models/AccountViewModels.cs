6701fecd2366999e0bd98821375b6d7616b212a3
sunisakc <sunisa_kan@gmail.com>
2018-01-10 15:49:16 +0100

    Changed

--- SchoolForum/SchoolForum/Models/AccountViewModels.cs
+++ SchoolForum/SchoolForum/Models/AccountViewModels.cs
@@ -7,7 +7,7 @@ namespace SchoolForum.Models
     {
         [Required]
         [Display(Name = "Email")]
-        public string Email { get; set; }
+        public string Email { get; set; } 
     }
 
     public class ExternalLoginListViewModel
