3b0110b610e17fa9bd2d28b45e78091464397727
Taleb01 <taleb_n1@hotmail.com>
2018-01-16 15:59:49 +0100

    UpdateView

--- SchoolForum/SchoolForum/Controllers/AccountController.cs
+++ SchoolForum/SchoolForum/Controllers/AccountController.cs
@@ -156,7 +156,7 @@ namespace SchoolForum.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
@@ -164,6 +164,7 @@ namespace SchoolForum.Controllers
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("Index", "SchoolForum");
+                     
                 }
                 AddErrors(result);
             }
