f70300ff4028352b1129559f669316744a0d8968
Taleb01 <taleb_n1@hotmail.com>
2018-01-16 16:54:43 +0100

    UpdateValidation in message model.

--- SchoolForum/SchoolForum/Models/Message.cs
+++ SchoolForum/SchoolForum/Models/Message.cs
@@ -11,20 +11,20 @@ namespace SchoolForum.Models
     public class Message
     {
         public int Id { get; set; }
-        
-        [Required(ErrorMessage ="Title is required")]
+
+        [Required(ErrorMessage = "Title is required")]
         [StringLength(30, MinimumLength = 3, ErrorMessage = "Title should be between 3 and 30 charachters.")]
-        [MaxWords(3, ErrorMessage ="Title should not be more than 3 words.")]
+        [MaxWords(3, ErrorMessage = "Title should not be more than 3 words.")]
         [DisplayName("Title")]
-        [DisplayFormat(NullDisplayText ="Undefined")]
+        [DisplayFormat(NullDisplayText = "Undefined")]
         [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Only Alphabets and Numbers allowed.")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage ="You should enter something.")]
+        [Required(ErrorMessage = "You should enter something.")]
         [StringLength(300, MinimumLength = 3, ErrorMessage = "Too long text.")]
         [MaxWords(100, ErrorMessage = "Text can not be more than 2 words.")]
         [DisplayName("Text")]
-        [DisplayFormat(NullDisplayText ="Undefined")]
+        [DisplayFormat(NullDisplayText = "Undefined")]
         public string Text { get; set; }
     }
 }
